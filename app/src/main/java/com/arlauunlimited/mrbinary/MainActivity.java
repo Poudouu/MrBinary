e8974308be8aa39a00c5297abc2d1c0bd467b765
Lau087 <lau_enl@hotmail.com>
2015-08-17 22:55:52 +0200

    Verry basic architecture
    Verry basic architecture

--- app/src/main/java/com/arlauunlimited/mrbinary/MainActivity.java
+++ app/src/main/java/com/arlauunlimited/mrbinary/MainActivity.java
@@ -3,22 +3,37 @@ package com.arlauunlimited.mrbinary;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void Switch_to_player_1(View view) {
+        Intent intent = new Intent(this, Player_1.class);
+        startActivity(intent);
+    }
+
+    public void Switch_to_player_2(View view) {
+        Intent intent = new Intent(this, Player_2.class);
+        startActivity(intent);
     }
 
     @Override
@@ -26,13 +41,17 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+        //return super.onOptionsItemSelected(item);
     }
 }
