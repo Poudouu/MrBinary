5b706c82a2f916c1b2eaba5037ba9c2b359d218e
Lau087 <lau_enl@hotmail.com>
2015-08-20 22:56:24 +0200

    Compare 2 strings functionality
    - Add of 2 buttons 1 and 0
    - Display the entered values
    - Comparison of the entered value with a test string 101 (5)
    - Display of a fail/win message and reset string at next input.

--- app/src/main/java/com/arlauunlimited/mrbinary/Player_1.java
+++ app/src/main/java/com/arlauunlimited/mrbinary/Player_1.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.view.View;
 
 /**
  * Created by i7-3930 on 17/08/2015.
@@ -18,11 +19,62 @@ public class Player_1 extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.player_1_layout);
         Intent intent = getIntent();
+        TextView output = (TextView)findViewById(R.id.output);
+        output.setText("");
     }
-    public void Modify_string_to_one() {
 
+    public void Modify_string_to_one(View vieuw) {
+        String test_to_compare ="101";
 
+        TextView output = (TextView)findViewById(R.id.output);
+        String temp_1 = (String) output.getText().toString();
+
+        if (((temp_1).equals("You win!!!!")) ||((temp_1).equals("You fail!!!!")) ){
+            temp_1="";
+        }
+
+        String temp_2=temp_1+"1";
+        int length_temp_2 = temp_2.length();
+        int length_test = test_to_compare.length();
+        if (length_temp_2 == length_test)
+        {
+            if ((test_to_compare).equals(temp_2)){
+                output.setText("You win!!!!");
+            }
+            else{
+                output.setText("You fail!!!!");
+            }
+        }else {
+            output.setText(temp_2);
+        }
     }
+
+    public void Modify_string_to_zero(View vieuw) {
+        String test_to_compare ="101";
+
+        TextView output = (TextView)findViewById(R.id.output);
+        String temp_1 = (String) output.getText().toString();
+
+        if (((temp_1).equals("You win!!!!")) ||((temp_1).equals("You fail!!!!")) ){
+            temp_1="";
+        }
+
+        String temp_2=temp_1+"0";
+        int length_temp_2 = temp_2.length();
+        int length_test = test_to_compare.length();
+        if (length_temp_2 == length_test)
+        {
+            if ((test_to_compare).equals(temp_2)){
+                output.setText("You win!!!!");
+            }
+            else{
+                output.setText("You fail!!!!");
+            }
+        }else {
+            output.setText(temp_2);
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
